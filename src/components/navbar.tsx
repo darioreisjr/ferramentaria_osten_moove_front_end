328d72e0b941329a54b98cdb49e91076d34fa9e6
Dario Reis <guttyerresjunior1995@gmail.com>
2023-09-29 23:22:20 -0300

    feat: color button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Osten Moove</span>
           </Link>
           <div className="flex md:order-2">
-            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button type="button" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
               <Link href="/newtool">
                 Register tool
               </Link>
